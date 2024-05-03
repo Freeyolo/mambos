b177e1d7be7245f5ea2aab8360e186721cf6ac3a
Freeyolo <102385399+Freeyolo@users.noreply.github.com>
2024-05-03 09:03:06 +0200

    t

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def get_matrikkel_data_syk(row):
         'version': '2.0.0',
         'request': 'GetFeature',
         'typename': 'app:Bygning',
-        'BygningstypeKodeEnumerationType': '111',
+        'simpletype': 'app:BygningstypeKodeEnumerationType=111',
         'srsname': 'EPSG:32633',
         'outputformat': 'application/gml+xml; version=3.2',
         'bbox': bbox_str,
